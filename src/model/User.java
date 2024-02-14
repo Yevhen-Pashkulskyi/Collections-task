9e19ba96f7eece6ee37528cbb89fd7defcf8a27f
Yevhen-Pashkulskyi <pashkulskyevhen@gmail.com>
2024-02-14 23:50:02 +0200

    add class user

---
+++ src/model/User.java
@@ -0,0 +1,22 @@
+package model;
+
+public class User {
+    private String name;
+    private String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public String tostring(){
+        return "Ім'я: " + name + ", Е-пошта: " + email;
+    }
+}
